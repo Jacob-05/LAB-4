ec482570db46c003e6c993d1c7b924f99aa39ff8
Jacob-05 <jacobosalazar320@gmail.com>
2025-05-02 18:57:41 -0500

    final commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-import ComponenteA from "./components/ComponenteA";
-import ComponenteB from "./components/ComponenteB";
-import ComponenteC from "./components/ComponenteC";
+import { YoutuberStore } from './flux/Store';
+import { YoutuberActions } from './flux/Actions';
+import { YoutubersContainer } from './components/YoutubersContainer';
 
-customElements.define('custom-element-a', ComponenteA);
-customElements.define('custom-element-b', ComponenteB);
-customElements.define('custom-element-c', ComponenteC);
+// Cargar datos iniciales
+fetch('/data/youtubers.json')
+    .then(response => response.json())
+    .then(data => {
+        const store = YoutuberStore.getInstance();
+        store.setYoutubers(data.youtubers);
+
+        // Inicializar el contenedor principal
+        const container = new YoutubersContainer(data.youtubers, 'app');
+        container.render();
+
+        // Suscribirse a cambios en el store
+        store.addChangeListener(() => {
+            container.update();
+        });
+    })
+    .catch(error => console.error('Error cargando datos:', error));
